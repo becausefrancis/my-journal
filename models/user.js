a1f2f9962dbd227a08b5eea356c0d29eddaafbe7
becausefrancis <jimenez.francis92@gmail.com>
2024-11-01 21:54:24 -0500

    Just need CSS left.

--- models/user.js
+++ models/user.js
@@ -1,5 +1,39 @@
 const mongoose = require('mongoose');
 
+const entrySchema = new mongoose.Schema({
+  month: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 12,
+  },
+  day: {
+    type: Number,
+    required: true,
+    min: 1,
+    max: 31,
+  },
+  weekday: {
+    type: String,
+    required: true,
+    enum: ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
+  },
+  task: {
+    type: String,
+  },
+  isComplete: {
+    type: Boolean,
+  },
+  journalEntry: {
+    type: String,
+    required: true,
+  },
+  status: {
+    type: String,
+    enum: ['positive', 'neutral', 'negative'],
+  },
+});
+
 const userSchema = mongoose.Schema({
   username: {
     type: String,
@@ -9,6 +43,7 @@ const userSchema = mongoose.Schema({
     type: String,
     required: true,
   },
+  entries: [entrySchema],
 });
 
 const User = mongoose.model('User', userSchema);
--- server.js
+++ server.js
@@ -7,19 +7,24 @@ const methodOverride = require('method-override');
 const morgan = require('morgan');
 const session = require('express-session');
 
+const isSignedIn = require('./middleware/is-signed-in.js');
+const passUserToView = require('./middleware/pass-user-to-view.js');
+
 const authController = require('./controllers/auth.js');
+const entriesController = require('./controllers/entries.js');
 
 const port = process.env.PORT ? process.env.PORT : '3000';
+const path = require('path');
 
 mongoose.connect(process.env.MONGODB_URI);
-
 mongoose.connection.on('connected', () => {
   console.log(`Connected to MongoDB ${mongoose.connection.name}.`);
 });
 
 app.use(express.urlencoded({ extended: false }));
 app.use(methodOverride('_method'));
-// app.use(morgan('dev'));
+app.use(morgan('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use(
   session({
     secret: process.env.SESSION_SECRET,
@@ -28,21 +33,20 @@ app.use(
   })
 );
 
-app.get('/', (req, res) => {
-  res.render('index.ejs', {
-    user: req.session.user,
-  });
-});
+app.use(passUserToView);
 
-app.get('/vip-lounge', (req, res) => {
+app.get('/', (req, res) => {
   if (req.session.user) {
-    res.send(`Welcome to the party ${req.session.user.username}.`);
+    res.redirect(`/users/${req.session.user._id}/entries`);
   } else {
-    res.send('Sorry, no guests allowed.');
+    res.render('index.ejs');
   }
 });
 
 app.use('/auth', authController);
+app.use(isSignedIn);
+
+app.use('/users/:userId/entries', entriesController);
 
 app.listen(port, () => {
   console.log(`The express app is ready on port ${port}!`);
